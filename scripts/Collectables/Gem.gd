814fbea944f1ffe5139299e4805d2d036abecf30
Diego Matos <59349414+diegomatos321@users.noreply.github.com>
2021-07-03 17:00:49 -0300

    efeitos sonoros da gema e do portal

--- scripts/Collectables/Gem.gd
+++ scripts/Collectables/Gem.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+onready var sfx:AudioStreamPlayer = get_node("SFX")
+onready var animationPlayer:AnimationPlayer = get_node("AnimationPlayer")
 
 func _on_Gem_body_entered(body: Node) -> void:
+	sfx.play()
+	animationPlayer.play("FadeOut")
+
+func collectCoin() -> void:
 	PlayerData.gems += 1
 	print(PlayerData.gems)
 	queue_free()
